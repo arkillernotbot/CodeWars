15feada94675acb6961ee03344be2ae2a889ecb3
Arki_ller <arnabroy072007@gmail.com>
2026-08-18 18:37:41 +0530

    Add solution - CodeHub

---
+++ python/6-kyu/stop-gninnips-my-sdrow/stop-gninnips-my-sdrow.py
@@ -0,0 +1,9 @@
+def spin_words(sentence):
+    words = sentence.split()
+    new_words = []
+    for word in words:
+        if len(word)>=5:
+            new_words.append(word[::-1])
+        else:
+            new_words.append(word)
+    return ' '.join(new_words)
